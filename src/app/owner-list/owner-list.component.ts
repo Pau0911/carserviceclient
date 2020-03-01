e9be397a110d843f8008589d14c2065f9a56260a
Paula Gómez <paulagomez@MacBook-Air-de-Paula.local>
2020-03-01 17:23:22 -0500

    Components

---
+++ src/app/owner-list/owner-list.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import {OwnerService} from '../shared/services/owner.service';
+import { CarService } from '../shared/car/car.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-owner-list',
+  templateUrl: './owner-list.component.html',
+  styleUrls: ['./owner-list.component.css']
+})
+export class OwnerListComponent implements OnInit {
+
+  owners: Array<any>;
+  owner: any;
+  selected: Array<any> = [] ;
+
+  constructor(private route: ActivatedRoute, private router: Router, private ownerService: OwnerService, private carService: CarService) { }
+
+  ngOnInit() {
+    this.ownerService.getAll().subscribe(data => {
+      this.owners = data._embedded.owners;
+    });
+  }
+
+  gotoList() {
+    this.router.navigate(['/owner-list']);
+  }
+  selectedItem(owner: any) {
+    this.selected.push(owner);
+    this.gotoList();
+  }
+
+  removeOwner() {
+    for (let owner of this.selected) {
+      this.ownerService.remove(owner._links.self.href).subscribe(data => {
+        let ownerDni = owner.dni;
+        this.carService.getAll().subscribe((result) => {
+          for (let car of result) {
+            if (car.ownerDni === ownerDni) {
+              car.ownerDni = null;
+              this.carService.save(car).subscribe(() => {
+              });
+            }
+          }
+        });
+        this.ngOnInit();
+      }, error => console.error(error));
+    }
+  }
+}
